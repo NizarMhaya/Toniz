d4517819776860b647821ce73dfdfed376da3612
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-10-25 09:40:45 +0200

    api_aliments GET fonctionne

---
+++ init_db.php
@@ -0,0 +1,9 @@
+<?php
+require_once('config.php'); // Inclut le fichier de configuration de la base de données
+require_once('template_connexion.php');
+
+// Exécute les commandes SQL depuis le fichier en utilisant file_get_contents
+$sqlCommands = file_get_contents('sql/aliments.sql');
+$pdo->exec($sqlCommands);
+
+echo 'La base de données a été initialisée avec succès.';
