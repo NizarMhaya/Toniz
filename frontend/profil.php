bc75e9bef38f7d023c543f316fc1073080449210
tsowden <tsowden.sup@gmail.com>
2023-10-27 10:18:22 +0200

    1er commit

---
+++ frontend/profil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil - Mon Site Web</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <?php require_once('template_menu.php'); 
+    renderMenuToHTML('profil'); ?>
+
+    <main>
+        <!-- Contenu de la page "profil.php" -->
+        <h1>Profil</h1>
+        <p>Ceci est la page de profil de l'utilisateur.</p>
+    </main>
+
+    <!-- Autres sections ou contenu ici -->
+
+    <footer>
+        <!-- Pied de page -->
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
