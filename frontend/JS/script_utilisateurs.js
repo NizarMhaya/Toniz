0c29fd73a48f3df02da0cfd65eb60f5440cfee23
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-11-02 15:04:35 +0100

    utilisateurs v1 GPT

--- frontend/JS/script_utilisateurs.js
+++ frontend/JS/script_utilisateurs.js
@@ -1,162 +1,56 @@
-$(document).ready(function () {
-    var table = $('#myTable').DataTable({
-        ajax: {
-            url: apiUrl,
-            dataSrc: ''
-        },
-        columns: [
-            { "data": "ID_USER" },
-            { "data": "NOM" },
-            { "data": "AGE" },
-            { "data": "SEXE" },
-            { "data": "SPORT" },
-            { "data": "KCAL_JOUR" },
-            { "data": null, "defaultContent": '<button class="btn btn-info btn-sm editBtn" id="edit">Edit</button>' },
-            { "data": null, "defaultContent": '<button class="btn btn-danger btn-sm deleteBtn" id="delete">Delete</button>' }
-        ]
+document.addEventListener('DOMContentLoaded', function () {
+    const ageInput = document.getElementById('age');
+    const sexeInput = document.getElementById('sexe');
+    const poidsInput = document.getElementById('poids');
+    const tailleInput = document.getElementById('taille');
+    const niveauActiviteInput = document.getElementById('niveauActivite');
+    const resultatDiv = document.getElementById('resultat');
+
+    [ageInput, sexeInput, poidsInput, tailleInput, niveauActiviteInput].forEach(input => {
+        input.addEventListener('input', calculerBesoinsEnergetiques);
     });
 
-    // Action lorsque le bouton submit est cliqué
-    $('#submitButton').on('click', function (e) {
-        e.preventDefault();
+    function calculerBesoinsEnergetiques() {
+        const age = parseInt(ageInput.value);
+        const sexe = sexeInput.value;
+        const poids = parseFloat(poidsInput.value);
+        const taille = parseFloat(tailleInput.value);
+        const niveauActivite = niveauActiviteInput.value;
 
-        // Récupérer les valeurs des champs du formulaire
-        var id = $('#idInput').val();
-        var nom = $('#nomInput').val();
-        var age = $('#ageInput').val();
-        var sexe = $('#sexeInput').val();
-        var sport = $('#sportInput').val();
-        var kcal = $('#kcalInput').val();
-
-        // Créer un nouvel objet avec les données du formulaire
-        var newData = {
-            "ID_USER": id,
-            "NOM": nom,
-            "AGE": age,
-            "SEXE": sexe,
-            "SPORT": sport,
-            "KCAL_JOUR": kcal
-        };
-
-        // Envoyer les données à votre API (remplacez avec votre méthode d'envoi de données)
-        $.ajax({
-            url: apiUrl, // Spécifiez l'URL de votre API pour ajouter des données
-            type: 'POST', // Méthode HTTP à utiliser (peut varier selon votre API)
-            data: JSON.stringify(newData), // Les données que vous voulez envoyer à l'API
-            dataType: 'json',
-            success: function (response) {
-                // Si l'ajout est réussi, ajoutez la nouvelle ligne à votre DataTable et redessinez-le
-                table.row.add(newData).draw();
-
-                // Réinitialisez les champs du formulaire
-                $('#idInput').val('');
-                $('#nomInput').val('');
-                $('#ageInput').val('');
-                $('#sexeInput').val('');
-                $('#sportInput').val('');
-                $('#kcalInput').val('');
-            },
-            error: function (error) {
-                // Gérez les erreurs si l'ajout échoue
-                console.error('Erreur lors de l\'ajout de la nouvelle ligne : ', error);
-            }
-        });
-    });
-
-
-    $('#myTable').on('click', '#delete', function () {
-        var row = $(this).closest('tr'); // Obtenez l'élément DOM de la ligne à supprimer
-        var data = table.row(row).data(); // Obtenez les données de la ligne
-
-        $.ajax({
-            url: apiUrl + '?id=' + data.ID_USER,
-            type: 'DELETE',
-            dataType: 'json',
-            success: function () {
-                console.log("Utilisateur supprimé avec succès :", data);
-
-                // Supprimez la ligne du DataTable en utilisant l'élément DOM de la ligne
-                table.row(row).remove().draw();
-            },
-            error: function (error) {
-                console.error('Erreur lors de la suppression de la ligne : ', error);
-            }
-        });
-    });
-
-
-    $('#myTable').on('click', '#edit', function () {
-        var row = $(this).closest('tr'); // Obtenez l'élément DOM de la ligne à éditer
-        var data = table.row(row).data(); // Obtenez les données de la ligne
-
-        // Remplissez votre formulaire de modification avec les données récupérées
-        $('#idInput').val(data.ID_USER);
-        $('#nomInput').val(data.NOM);
-        $('#ageInput').val(data.age);
-        $('#sexeInput').val(data.sexe);
-        $('#sportInput').val(data.sport);
-        $('#kcalInput').val(data.kcal_100G);
-
-        // Affichez le bouton "Enregistrer" et masquez le bouton "Soumettre"
-        $('#saveButton').show();
-        $('#submitButton').hide();
-
-        // Attachez un gestionnaire d'événements au bouton "Enregistrer" pour appliquer les modifications
-        $('#saveButton').off('click').on('click', function () {
-            var newid = $('#idInput').val();
-            var newNom = $('#nomInput').val();
-            var newage = $('#ageInput').val();
-            var newsexe = $('#sexeInput').val();
-            var newsport = $('#sportInput').val();
-            var newkcal = $('#kcalInput').val();
-
-            var formData = {
-                "ID_USER": newid,
-                "NOM": newNom,
-                "age": newage,
-                "sexe": newsexe,
-                "sport": newsport,
-                "kcal_100G": newkcal
+        if (age && poids && taille) {
+            let bmr;
+            if (sexe === 'homme') {
+                bmr = 88.362 + (13.397 * poids) + (4.799 * taille) - (5.677 * age);
+            } else {
+                bmr = 447.593 + (9.247 * poids) + (3.098 * taille) - (4.330 * age);
             }
 
-            // Envoyez une requête PUT à votre API pour appliquer les modifications
-            $.ajax({
-                type: "PUT",
-                url: apiUrl,
-                contentType: "application/json",
-                data: JSON.stringify(formData),
-                success: function (data) {
-                    console.log("Utilisateur mis à jour avec succès :", data);
+            let tdee;
+            switch (niveauActivite) {
+                case 'sedentaire':
+                    tdee = bmr * 1.2;
+                    break;
+                case 'legerement_actif':
+                    tdee = bmr * 1.375;
+                    break;
+                case 'moderement_actif':
+                    tdee = bmr * 1.55;
+                    break;
+                case 'tres_actif':
+                    tdee = bmr * 1.725;
+                    break;
+                case 'extremement_actif':
+                    tdee = bmr * 1.9;
+                    break;
+                default:
+                    tdee = bmr * 1.2; // Par défaut, considérer un niveau sédentaire
+            }
 
-                    // Mettez à jour les données de la ligne dans le DataTable
-                    table.row(row).data(formData).draw();
-
-                    // Réinitialisez les champs du formulaire de modification et masquez le bouton "Enregistrer"
-                    $('#idInput').val('');
-                    $('#nomInput').val('');
-                    $('#ageInput').val('');
-                    $('#sexeInput').val('');
-                    $('#sportInput').val('');
-                    $('#kcalInput').val('');
-                    $('#saveButton').hide();
-                    $('#submitButton').show();
-                },
-                error: function (error) {
-                    // Gérez les erreurs si la modification échoue
-                    console.error('Erreur lors de la modification de la ligne : ', error);
-                }
-            });
-        });
-    });
-
-
-
-
-
-
-
-
-
-
+            // Afficher le résultat
+            resultatDiv.textContent = `Besoins Énergétiques Journaliers : ${tdee.toFixed(2)} calories`;
+        } else {
+            // Si les champs ne sont pas tous remplis, afficher un message d'erreur
+            resultatDiv.textContent = "Veuillez remplir tous les champs.";
+        }
+    }
 });
-
--- frontend/utilisateurs.php
+++ frontend/utilisateurs.php
@@ -1,101 +1,48 @@
-<!doctype html>
-<html lang="fr">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset='utf-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js" crossorigin="anonymous"></script>
-    <title>tabletest</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-
-
-    <style>
-        body {
-            margin-top: 5em;
-        }
-
-        .table {
-            margin-top: 100px;
-            margin-bottom: 100px;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculateur de Besoins Énergétiques</title>
+    <link rel="stylesheet" href="CSS/styles.css">
 </head>
 
 <body>
-    <table id="myTable">
-        <thead>
-            <tr>
-                <th scope="col">ID_USER</th>
-                <th scope="col">NOM</th>
-                <th scope="col">AGE</th>
-                <th scope="col">SEXE</th>
-                <th scope="col">SPORT</th>
-                <th scope="col">KCAL_JOUR</th>
-                <th scope="col">Edit</th> <!-- Colonne pour le bouton Edit -->
-                <th scope="col">Delete</th> <!-- Colonne pour le bouton Delete -->
-            </tr>
-        </thead>
-        <tbody id="studentsTableBody">
-        </tbody>
-    </table>
-    <form id="addStudentForm">
-        <div class="form-group row">
-            <div class="form-group row">
-                <label for="idInput" class="col-sm-2 col-form-label">ID_USER*</label>
-                <div class="col-sm-3">
-                    <input type="text" class="form-control" id="idInput">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="nomInput" class="col-sm-2 col-form-label">NOM*</label>
-                <div class="col-sm-3">
-                    <input type="text" class="form-control" id="nomInput">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="ageInput" class="col-sm-2 col-form-label">AGE*</label>
-                <div class="col-sm-3">
-                    <input type="text" class="form-control" id="ageInput">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="sexeInput" class="col-sm-2 col-form-label">SEXE*</label>
-                <div class="col-sm-3">
-                    <input type="text" class="form-control" id="sexeInput">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="sportInput" class="col-sm-2 col-form-label">SPORT*</label>
-                <div class="col-sm-3">
-                    <input type="text" class="form-control" id="sportInput">
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="kcalInput" class="col-sm-2 col-form-label">KCAL_JOUR*</label>
-                <div class="col-sm-3">
-                    <input type="text" class="form-control" id="kcalInput">
-                </div>
-            </div>
-            <div class="form-group row">
-                <span class="col-sm-2"></span>
-                <div class="col-sm-2">
-                    <button type="button" class="btn btn-primary form-control" id="submitButton" style="display: block;">Submit</button>
-                    <!--type="submit" : Lorsque ce bouton est cliqué à l'intérieur d'un formulaire, le formulaire est soumis au serveur pour traitement. Cela signifie que les données du formulaire sont envoyées au serveur pour être traitées par un script ou un programme. -->
-                    <!--type="button" : Cela signifie que le bouton est un bouton ordinaire. Il n'a pas de comportement par défaut lorsqu'il est cliqué. Vous pouvez définir un comportement personnalisé en utilisant JavaScript-->
-                </div>
-            </div>
-    </form>
-    <button type="button" id="saveButton" class="btn btn-primary " style="display: none;">Enregistrer</button>
-
-    <script>
-        let apiUrl = "<?php require_once 'config.php'; // j'utilise en chemin relatif vers config dont le but est de ne plus utiliser de lien en dur pour l'API...
-                        echo _API_URL_UTILISATEURS; ?> "; // utilisation de la variable définie dans config
-    </script>
-    <script src="JS/script_utilisateurs.js" defer></script>
-
+    <div class="container">
+        <h1>Calculateur de Besoins Énergétiques</h1>
+        <div class="form-group">
+            <label for="age">Âge :</label>
+            <input type="number" id="age" placeholder="Entrez votre âge">
+        </div>
+        <div class="form-group">
+            <label for="sexe">Sexe :</label>
+            <select id="sexe">
+                <option value="homme">Homme</option>
+                <option value="femme">Femme</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="poids">Poids (kg) :</label>
+            <input type="number" id="poids" placeholder="Entrez votre poids">
+        </div>
+        <div class="form-group">
+            <label for="taille">Taille (cm) :</label>
+            <input type="number" id="taille" placeholder="Entrez votre taille">
+        </div>
+        <div class="form-group">
+            <label for="niveauActivite">Niveau d'activité :</label>
+            <select id="niveauActivite">
+                <option value="sedentaire">Sédentaire</option>
+                <option value="legerement_actif">Légèrement Actif</option>
+                <option value="moderement_actif">Modérément Actif</option>
+                <option value="tres_actif">Très Actif</option>
+                <option value="extremement_actif">Extrêmement Actif</option>
+            </select>
+        </div>
+        <div id="resultat"></div>
+    </div>
+    <script src="JS/script_utilisateurs.js"></script>
 </body>
 
 </html>
