7196e4426007575b91fb9979502ed13f255db27c
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-11-03 17:28:23 +0100

    favicon aoe toutes pages

--- frontend/calculateur.php
+++ frontend/calculateur.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculateur de Besoins Énergétiques</title>
     <link rel="stylesheet" href="CSS/styles_calculateur.css">
+    <link rel="icon" type="image/x-icon" href="assets/faviconV2.png" />
     <?php require_once('template_menu.php'); ?>
     <header class="bg-dark py-1">
         <?php renderMenuToHTML('ajouter_favoris'); ?>
--- frontend/front_aliments_ajouter_favoris.php
+++ frontend/front_aliments_ajouter_favoris.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <link rel="stylesheet" href="CSS/styles.css">
-
+    <link rel="icon" type="image/x-icon" href="assets/faviconV2.png" />
     <?php require_once('template_menu.php'); ?>
     <header class="bg-dark py-1">
         <?php renderMenuToHTML('ajouter_favoris'); ?>
