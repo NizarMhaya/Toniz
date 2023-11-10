2da74bf796e94e55abe04240405b0498b38c9035
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-11-10 16:23:39 +0100

    commit

--- backend/api_aliments_ajouter_favoris.php
+++ backend/api_aliments_ajouter_favoris.php
@@ -5,49 +5,6 @@
 header('Content-Type: application/json'); // Définir le type de contenu JSON
 
 require_once('init_pdo.php');
-//Ce code sera à utiliser pour afficher seulement les favoris. Or ici je veux afficher tous les aliments donc je reprends le meme get que api_aliments_reel
-// if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-//     // Vérifiez l'ID de l'utilisateur à partir des données de la session ou du token d'authentification
-//     $userID = 1; // Remplacez ceci par la méthode appropriée pour obtenir l'ID de l'utilisateur
-
-//     // Utilisez la fonction get_aliments_favoris pour obtenir les aliments favoris de l'utilisateur
-//     $alimentsFavoris = get_aliments_favoris($pdo, $userID);
-
-//     // Vérifiez si des aliments favoris ont été trouvés
-//     if ($alimentsFavoris) {
-//         http_response_code(200); // OK
-//         echo json_encode($alimentsFavoris);
-//     } else {
-//         http_response_code(404); // Not Found
-//         echo json_encode(array('error' => 'Aucun aliment favori trouvé.'));
-//     }
-// }
-
-// // Fonction pour récupérer les aliments favoris de l'utilisateur
-// function get_aliments_favoris($pdo, $userID)
-// {
-//     try {
-//         // Préparez la requête SQL pour obtenir les aliments favoris de l'utilisateur
-//         $stmt = $pdo->prepare("SELECT a.CODE_BARRES, a.NOM, a.MARQUE, a.CATEGORIE, a.ENERGIE_100G
-//                               FROM aliment a
-//                               JOIN aliments_favoris af ON a.CODE_BARRES = af.CODE_BARRES
-//                               WHERE af.ID_USER = :user_id");
-
-//         // Liez la valeur de l'ID de l'utilisateur en tant que paramètre
-//         $stmt->bindParam(':user_id', $userID, PDO::PARAM_INT);
-
-//         // Exécutez la requête
-//         $stmt->execute();
-
-//         // Récupérez les aliments favoris de l'utilisateur
-//         $alimentsFavoris = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-//         return $alimentsFavoris;
-//     } catch (PDOException $e) {
-//         // En cas d'erreur, retournez un message d'erreur
-//         return array('error' => 'Erreur lors de la récupération des aliments favoris : ' . $e->getMessage());
-//     }
-// }
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
--- frontend/config.php
+++ frontend/config.php
@@ -5,13 +5,9 @@ define('_API_URL_PROFIL', 'http://localhost/Toniz/backend/api_profil.php');
 define('_API_URL_DECO', 'http://localhost/Toniz/backend/logout.php');
 define('_API_URL_SIGNIN', 'http://localhost/Toniz/backend/api_inscription.php');
 
-
-define('_API_URL_UTILISATEURS', 'http://localhost/Toniz/backend/api_utilisateurs.php');
 define('_API_URL_ALIMENT_FAVORIS', 'http://localhost/Toniz/backend/api_aliments_ajouter_favoris.php');
 define('_API_URL_FAVORIS', 'http://localhost/Toniz/backend/api_favoris.php');
 define('_API_URL_VOIR_REPAS', 'http://localhost/Toniz/backend/api_voir_repas.php');
 define('_API_URL_NEW_REPAS', 'http://localhost/Toniz/backend/api_repas.php');
 define('_API_URL_INTEGRATION', 'http://localhost/Toniz/backend/api_aliments_integration.php');
 define('_API_URL_NUTRIMENT', 'http://localhost/Toniz/backend/api_nutriment.php');
-
-
