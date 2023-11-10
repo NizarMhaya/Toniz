ea81e6d5bf07a8834a9a6f0d7559b6b93caa1b73
MHAYA Nizar <nizar.mhaya@gmail.com>
2023-11-10 10:37:38 +0100

    footer réparé

--- frontend/repas.php
+++ frontend/repas.php
@@ -6,41 +6,42 @@
 
 
 </head>
+<main>
 
-<body>
-    <?php require_once('template_menu.php'); ?>
-    <header class="bg-dark py-1">
-        <?php renderMenuToHTML('aliments'); ?>
-    </header>
+    <body>
+        <?php require_once('template_menu.php'); ?>
+        <header class="bg-dark py-1">
+            <?php renderMenuToHTML('repas'); ?>
+        </header>
 
 
-    <h1 class="my-custom-h1">Créez votre repas</h1>
-    <h2 id="custom-description">Entrez le nom et la date du repas et ajoutez autant d'aliments que vous le souhaitez en précisant leur quantités.</h2>
+        <h1 class="my-custom-h1">Créez votre repas</h1>
+        <h2 id="custom-description">Entrez le nom et la date du repas et ajoutez autant d'aliments que vous le souhaitez en précisant leur quantités.</h2>
 
-    <form id="formulaire-repas">
-        <label for="nom-repas">Nom du repas :</label>
-        <input type="text" id="nom-repas" name="nom-repas" required><br><br>
+        <form id="formulaire-repas">
+            <label for="nom-repas">Nom du repas :</label>
+            <input type="text" id="nom-repas" name="nom-repas" required><br><br>
 
-        <label for="date-repas">Date du repas :</label>
-        <input type="datetime-local" id="date-repas" name="date-repas" required><br><br>
+            <label for="date-repas">Date du repas :</label>
+            <input type="datetime-local" id="date-repas" name="date-repas" required><br><br>
 
-        <div id="aliments-container">
-            <!-- Les champs d'ajout d'aliments seront ajoutés ici dynamiquement -->
-        </div>
+            <div id="aliments-container">
+                <!-- Les champs d'ajout d'aliments seront ajoutés ici dynamiquement -->
+            </div>
 
-        <button type="button" id="ajouter-aliment">Ajouter un aliment</button><br><br>
+            <button type="button" id="ajouter-aliment">Ajouter un aliment</button><br><br>
 
-        <button type="submit" id="enregistrer-repas-button">Enregistrer le repas</button>
-    </form>
+            <button type="submit" id="enregistrer-repas-button">Enregistrer le repas</button>
+        </form>
 
-    <script>
-        const alimentsContainer = document.getElementById("aliments-container");
-        const ajouterAlimentButton = document.getElementById("ajouter-aliment");
-        let alimentIndex = 1;
+        <script>
+            const alimentsContainer = document.getElementById("aliments-container");
+            const ajouterAlimentButton = document.getElementById("ajouter-aliment");
+            let alimentIndex = 1;
 
-        ajouterAlimentButton.addEventListener("click", function() {
-            const nouvelAlimentDiv = document.createElement("div");
-            nouvelAlimentDiv.innerHTML = `
+            ajouterAlimentButton.addEventListener("click", function() {
+                const nouvelAlimentDiv = document.createElement("div");
+                nouvelAlimentDiv.innerHTML = `
             <label for="aliment-${alimentIndex}">Aliment (code barres) :</label>
             <input type="number" class="aliment-input" id="aliment-${alimentIndex}" name="aliment-${alimentIndex}" required><br><br>
 
@@ -48,41 +49,20 @@
             <input type="number" class="quantite-input" id="quantite-${alimentIndex}" name="quantite-${alimentIndex}" required><br><br>
         `;
 
-            alimentsContainer.appendChild(nouvelAlimentDiv);
-            alimentIndex++;
-        });
-    </script>
+                alimentsContainer.appendChild(nouvelAlimentDiv);
+                alimentIndex++;
+            });
+        </script>
 
-    <button id="toggleButton">Afficher les aliments favoris</button>
+        <button id="toggleButton">Afficher les aliments favoris</button>
 
 
 
-    <div id="secondTableContainer" class="table-container" style="display: none;">
-        <h1 class="my-custom-h1">Vos aliments favoris</h1>
-        <h2 id="custom-description">Votre sélection favorite</h2>
-        <table id="secondTable">
-            <!-- Contenu du deuxième tableau -->
-            <thead>
-                <tr>
-                    <th scope="col">Copier le code-barres</th>
-                    <th scope="col">CODE BARRES</th>
-                    <th scope="col">NOM</th>
-                    <th scope="col">MARQUE</th>
-                    <th scope="col">CATEGORIE</th>
-                    <th scope="col">ENERGIE_100G</th>
-                </tr>
-            </thead>
-
-        </table>
-    </div>
-    </div>
-
-    <div class="container">
-        <div id="firstTableContainer" class="table-container">
-            <h1 class="my-custom-h1">Tableau des aliments</h1>
-            <h2 id="custom-description">Recopiez le code barre des aliments de votre choix</h2>
-            <table id="myTable">
-                <!-- Contenu du premier tableau -->
+        <div id="secondTableContainer" class="table-container" style="display: none;">
+            <h1 class="my-custom-h1">Vos aliments favoris</h1>
+            <h2 id="custom-description">Votre sélection favorite</h2>
+            <table id="secondTable">
+                <!-- Contenu du deuxième tableau -->
                 <thead>
                     <tr>
                         <th scope="col">Copier le code-barres</th>
@@ -96,29 +76,50 @@
 
             </table>
         </div>
+        </div>
+
+        <div class="container">
+            <div id="firstTableContainer" class="table-container">
+                <h1 class="my-custom-h1">Tableau des aliments</h1>
+                <h2 id="custom-description">Recopiez le code barre des aliments de votre choix</h2>
+                <table id="myTable">
+                    <!-- Contenu du premier tableau -->
+                    <thead>
+                        <tr>
+                            <th scope="col">Copier le code-barres</th>
+                            <th scope="col">CODE BARRES</th>
+                            <th scope="col">NOM</th>
+                            <th scope="col">MARQUE</th>
+                            <th scope="col">CATEGORIE</th>
+                            <th scope="col">ENERGIE_100G</th>
+                        </tr>
+                    </thead>
+
+                </table>
+            </div>
 
 
-        </main>
+</main>
 
-        <script>
-            let apiUrl = "<?php require_once 'config.php'; // j'utilise en chemin relatif vers config dont le but est de ne plus utiliser de lien en dur pour l'API...
-                            echo _API_URL_INTEGRATION; ?> "; // utilisation de la variable définie dans config
-        </script>
-        <script src="JS/script_aliments_integration.js" defer></script>
-        <script src="JS/script_repas.js" defer></script>
-        <script>
-            let apiUrlRepas = "<?php require_once 'config.php'; // j'utilise en chemin relatif vers config dont le but est de ne plus utiliser de lien en dur pour l'API...
-                                echo _API_URL_NEW_REPAS; ?> "; // utilisation de la variable définie dans config
-        </script>
-        <script>
-            let apiUrlFavoris = "<?php require_once 'config.php'; // j'utilise en chemin relatif vers config dont le but est de ne plus utiliser de lien en dur pour l'API...
-                                    echo _API_URL_FAVORIS; ?> "; // utilisation de la variable définie dans config
-        </script>
+<script>
+    let apiUrl = "<?php require_once 'config.php'; // j'utilise en chemin relatif vers config dont le but est de ne plus utiliser de lien en dur pour l'API...
+                    echo _API_URL_INTEGRATION; ?> "; // utilisation de la variable définie dans config
+</script>
+<script src="JS/script_aliments_integration.js" defer></script>
+<script src="JS/script_repas.js" defer></script>
+<script>
+    let apiUrlRepas = "<?php require_once 'config.php'; // j'utilise en chemin relatif vers config dont le but est de ne plus utiliser de lien en dur pour l'API...
+                        echo _API_URL_NEW_REPAS; ?> "; // utilisation de la variable définie dans config
+</script>
+<script>
+    let apiUrlFavoris = "<?php require_once 'config.php'; // j'utilise en chemin relatif vers config dont le but est de ne plus utiliser de lien en dur pour l'API...
+                            echo _API_URL_FAVORIS; ?> "; // utilisation de la variable définie dans config
+</script>
 
-        <footer class="py-4 bg-dark">
-            <?php require_once('template_footer.php'); ?>
+<footer class="py-4 bg-dark">
+    <?php require_once('template_footer.php'); ?>
 
-        </footer>
+</footer>
 </body>
 
 </html>
